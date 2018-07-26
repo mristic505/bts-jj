207bf2662683de72ed4ad5bfe6d5478b43ee0157
Mateja Ristic <mristic@brandconnections.com>
2018-07-26 21:49:53 +0200

    fix matching numbers BG

--- db.php
+++ db.php
@@ -4,3 +4,7 @@ require_once 'meekrodb.2.3.class.php';
 DB::$user = 'juicyjuice';
 DB::$password = 'iYH0ki2fY0CcvgIS';
 DB::$dbName = 'wp_juicyjuice';
+
+// DB::$user = 'root';
+// DB::$password = 'root';
+// DB::$dbName = 'bts';
