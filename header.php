2dde1264bf54ca7eadb015ed2573caaba7090bb8
Milos Tosic <milos.tosic.rs@gmail.com>
2018-08-06 18:38:12 +0200

    Possibly dirty fix to fix mobile hamburger menu

--- header.php
+++ header.php
@@ -65,15 +65,15 @@ document.write('<iframe src="https://8096545.fls.doubleclick.net/activityi;src=8
 		<nav class="navbar navbar-default">
 			  <div class="container-fluid">			  	
 			    <!-- Brand and toggle get grouped for better mobile display -->
-			    <div class="navbar-header">			      
-			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			    <div class="navbar-header">	
+			    </div>
+		      
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 			        <span class="sr-only">Toggle navigation</span>
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
-			      </button>
-			    </div>
-
+					</button>
 			    <!-- Collect the nav links, forms, and other content for toggling -->
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			    <img class="logo menu-logo" src="img/mobile-menu-logo.png">
