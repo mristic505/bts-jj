840aa1aa937c915949b6de4e94e3e6ba1d4b6110
Mateja Ristic <mristic@brandconnections.com>
2018-08-10 14:43:39 -0400

    Final updates

--- js/subprices-ccw.js
+++ js/subprices-ccw.js
@@ -345,7 +345,7 @@ jQuery(document).ready(function($) {
         end_fruit = 'kiwi_str';
     }
     if (play == 'pcf') {
-        $('#roulette').prepend('<div style="z-index: 99999;" id="register"> <div class="clearfix prc_holder"> <h1 class="h1_title">Congratulations!</h1> <img class="prc_boy_big" src="img/boy_big.png"> <div class="prc_wheel_copy"> <div class="prc1">You are a potential prizewinner.</div><div class="prc2">As soon as we receive your prize claim form and verify your compliance with the Official Rules and game play validation, you will be named an OFFICIAL WINNER! <br><br>To get started, please complete: </div><a class="spin_btn prc_btn" href="?page=prize-claim-form">PRIZE CLAIM FORM</a> </div></div><div class="ghost"></div></div>');
+        $('#roulette').prepend('<div style="z-index: 99999;" id="register"> <div class="clearfix prc_holder"> <h1 class="h1_title">Congratulations!</h1> <img class="prc_boy_big" src="img/boy_big.png"> <div class="prc_wheel_copy"> <div class="prc1">You are a potential prize winner.</div><div class="prc2">As soon as we receive your prize claim form and verify your compliance with the Official Rules and game play validation, you will be named an OFFICIAL WINNER! <br><br>To get started, please complete: </div><a class="spin_btn prc_btn" href="?page=prize-claim-form">PRIZE CLAIM FORM</a> </div></div><div class="ghost"></div></div>');
         $('.spin_holder').after('<div  class="overlay"></div>');
         $('#roulette .spinner').hide();
     }
