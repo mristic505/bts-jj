493ca3de4f486843def55090154f3510b810d280
Mateja Ristic <mristic@brandconnections.com>
2018-08-17 10:52:40 -0400

    download changes from production

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link type="text/css" rel="stylesheet" media="all" href="css/style.css?ver=2.4.0"></link>
+<link type="text/css" rel="stylesheet" media="all" href="css/style.css?ver=2.4.1"></link>
 <!--[if IE]>
 	<link rel="stylesheet" type="text/css" href="css/all-ie-only.css" />
 <![endif]-->
